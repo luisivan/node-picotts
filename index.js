3e1284c58846a2ca039a2c49a4b240d31a484830
Luis Iván <me@luisivan.net>
2014-04-23 21:15:48 +0200

    Stupid fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function say(text, lang, cb) {
 	var file = getTmpFile(),
 		command = commandTmpl.replace('{{lang}}', lang).replace('{{text}}', text).replace(/\{\{file\}\}/g, file)
 	exec(command, function(err) {
-		cb(err)
+		cb && cb(err)
 		fs.unlink(file)
 	})
 }
